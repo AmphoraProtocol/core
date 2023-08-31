b88420be3c8668dc70ffbb6293c8f17e05363cbb
OpenOrg-gg <adam@openorg-gg>
2023-08-31 11:20:01 -0400

    fix: checksum2

--- solidity/test/utils/TokenAddresses.sol
+++ solidity/test/utils/TokenAddresses.sol
@@ -4,7 +4,7 @@ pragma solidity >=0.8.0;
 contract TokenAddresses {
   // Token addresses
   address public constant WETH_ADDRESS = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-  address public constant SUSD_ADDRESS = 0xb2f30a7c980f052f02563fb518dcc39e6bf38175;
+  address public constant SUSD_ADDRESS = 0xb2F30A7C980f052f02563fb518dcc39e6bf38175;
   address public constant USDT_ADDRESS = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
   address public constant UNI_ADDRESS = 0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984;
   address public constant AAVE_ADDRESS = 0x7Fc66500c84A76Ad7e9c93437bFc5Ac33E2DDaE9;
